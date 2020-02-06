a9b295c58e9a5fc11126303c799c66188b55692e
esojourn <esojourn@gmail.com>
2020-02-06 15:28:26 +0800

    init version
    基本功能完成。细节待优化

--- index.php
+++ index.php
@@ -2,32 +2,10 @@
 ini_set('display_errors', 1); 
 ini_set('display_startup_errors', 1); 
 error_reporting(E_ALL);
-?>
-<!DOCTYPE html>  
-
-<html lang="zh-CN" class="no-js">
-<head>  
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, maximum-scale=1">
-    <title>敬拜赞美</title>  
-
-    <link rel="stylesheet" type="text/css" href="styles.css"/>     
-    <link rel="stylesheet" type="text/css" href="DataTables-1.10.20/css/jquery.dataTables.min.css"/>
-     
-    <script type="text/javascript" src="jQuery-3.3.1/jquery-3.3.1.min.js"></script>
-    <script type="text/javascript" src="DataTables-1.10.20/js/jquery.dataTables.min.js"></script>
-
-</head>  
-<body>  
-
-<?php
-
-if (isset($_GET["n"])){
-    $n=htmlspecialchars($_GET["n"]);
-}
-//$n=(isset($_GET["n"])) ? htmlspecialchars($_GET["n"]) : NULL;
 
 include 'lib.php';
+ 
+print_html_header();
 
 $path    = 'data';
 $data = scandir($path);
@@ -54,80 +32,35 @@ foreach ($data as $key => $value) {
     }
 }
 
-$output = '<table id="mytable"><thead><tr><th>诗歌</th></tr></thead><tbody>';
+$output = get_html_contenttable($name, $audio);
 
-foreach ($name as $k1 => $v1) {
-    foreach ($audio as $k2 => $v2) {
-        if($name[$k1]['id']==$audio[$k2]['id']){
-            $name[$k1]['matched'] =TRUE;
-        }
-    }
-    $output .= "<tr><td>\n";
-    $output .= '<a href="?n='.$v1['id'].'">' .$v1['name']."</a>\n";
-    $output .= "</td></tr>\n";
-}
-$output .= '</tbody></table>';
-
-
-
-/*
-foreach ($filesAll as $key => $value) {
-    $namestr = mb_substr($value, 0, mb_strlen($value-4));
-    $namestr = mb_strlen($value);
-    echo $namestr . '<br>';
-}
-*/
-
-//echo (substr($filesAll[0],-4) == '.mp3') ?  'yes' : 'no';
-    
 echo '<pre>';
-
 //var_dump($name);
 //var_dump($audio);
 echo '</pre>';
 
+if (isset($_GET["n"])){
+    $n=htmlspecialchars($_GET["n"]);
+}else{
+    $n='';
+}
+//$n=(isset($_GET["n"])) ? htmlspecialchars($_GET["n"]) : NULL;
 
+$playkey=is_valid_id($name, $n);
+//var_dump($playkey);
+
+preg_match("/^[0-9]{2}-[0-9]{2,3}/", $n);
 
 if($n=='index' || $n=='i' || $n==''){
     echo '<div class="main">';
     echo $output;
     echo '</div>';
-
+}elseif($playkey !== FALSE){ //如果ID符合格式
+    print_html_player($name[$playkey]);
 }else{
-
-?>
-<div class='main'>
-    <div class='audio clearfix'>
-    <audio controls autoplay>  
-        <source src="<?php echo $n.'.mp3';?>" type="audio/mpeg">  
-    </audio>  
-</div>
-
-<div class='img'>
-    <img src="<?php echo $n.'.png';?>" />
-    <h1 class='content'><a href='?n=i'>回目录</a></h1>
-</div>
-</div>
-
-<?php
+    echo '<h1>无权访问</h1>';
 }
 ?>
-<script type="text/javascript">
-$(document).ready( function () {
-    $('#mytable').DataTable({
-        "language": {
-            "search": "搜索：",
-            "lengthMenu":     "每页显示 _MENU_ 首",
-            "info": "显示第 _START_ 至 _END_ 首。共 _TOTAL_ 首",
-                "paginate": {
-                "first":      "首页",
-                "last":       "末页",
-                "next":       "向后",
-                "previous":   "向前"
-            },
-        }
-    });
-} );
-</script>
+
 </body>  
 </html> 
--- lib.php
+++ lib.php
@@ -1,4 +1,7 @@
 <?php
+/*
+ * 根据目录中所有文件，整理出格式化的数组。
+*/
 function get_name_array($value, $path){
 
     $filesTxt[] = $value;
@@ -16,4 +19,143 @@ function get_name_array($value, $path){
     ];
     unset($arr, $subid, $id);
     return $name;
+}
+
+
+/*
+ * 建立表格字符串。同时更新数组内 matched 字段信息。
+*/
+function get_html_contenttable(&$name, $audio){
+    $output = '<table id="mytable"><thead><tr><th>诗歌</th></tr></thead><tbody>';
+
+    foreach ($name as $k1 => $v1) {
+        foreach ($audio as $k2 => $v2) {
+            if($name[$k1]['id']==$audio[$k2]['id']){  //如果txt文件有对应的mp3，则更新matched为true
+                $name[$k1]['mp3'] = $audio[$k2]['path'];
+                $name[$k1]['matched'] =TRUE;
+            }
+        }
+        
+        $output .= "<tr><td>\n";
+        if ($name[$k1]['matched'] == TRUE) {
+            $output .= '<a href="?n='.$v1['id'].'">' .$v1['name']."</a>\n";
+        }else{
+            $output .= $v1['name']." - 无伴奏\n";
+        }
+        $output .= "</td></tr>\n";
+    }
+    $output .= '</tbody></table>';
+    $output .= <<<EOL
+<script type="text/javascript">
+$(document).ready( function () {
+    $('#mytable').DataTable({
+        "language": {
+            "search": "搜索：",
+            "lengthMenu":     "每页显示 _MENU_ 首",
+            "info": "显示第 _START_ 至 _END_ 首。共 _TOTAL_ 首",
+                "paginate": {
+                "first":      "首页",
+                "last":       "末页",
+                "next":       "向后",
+                "previous":   "向前"
+            },
+        }
+    });
+} );
+</script>
+EOL;
+
+    return $output;
+}
+
+
+function is_valid_id($name, $n){
+    foreach ($name as $key => $value) {
+        if($n == $value['id']){
+            return $key;
+        }
+    }
+    return FALSE;
+}
+
+
+function print_html_header(){
+    echo <<<EOL
+    <!DOCTYPE html>  
+
+    <html lang="zh-CN" class="no-js">
+    <head>  
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, maximum-scale=1">
+        <title>敬拜赞美</title>  
+
+        <link rel="stylesheet" type="text/css" href="styles.css"/>     
+        <link rel="stylesheet" type="text/css" href="DataTables-1.10.20/css/jquery.dataTables.min.css"/>
+         
+        <script type="text/javascript" src="jQuery-3.3.1/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="DataTables-1.10.20/js/jquery.dataTables.min.js"></script>
+
+    </head>  
+    <body>
+EOL;
+}
+
+function PageURL() 
+{
+  $pageURL = 'http';
+  if(isset($_SERVER["HTTPS"])){
+       if ($_SERVER["HTTPS"] == "on") 
+      {
+        $pageURL .= "s";
+      }
+  }
+
+  $pageURL .= "://";
+ 
+  if ($_SERVER["SERVER_PORT"] != "80") 
+  {
+    $pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
+  } 
+  else
+  {
+    $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+  }
+  return $pageURL;
+}
+
+function print_html_player($name){
+    //var_dump($name);
+    $mp3 = $name["mp3"];
+    $id = $name["id"];
+    $pageURL = PageURL() ;
+
+    $file = file_get_contents($name["path"]);
+
+    $encode = mb_detect_encoding($file, array("ASCII",'UTF-8',"GB2312","GBK",'BIG5','LATIN1'));
+    if($encode != 'UTF-8'){
+        $file = mb_convert_encoding($file, 'UTF-8', $encode);
+    }
+
+    
+
+    echo <<<EOL
+    <div class='main'>
+        <div class='audio clearfix'>
+        <audio controls autoplay>  
+            <source src="$mp3" type="audio/mpeg">  
+        </audio>  
+    </div>
+
+    <div class='text'><pre>
+        $file
+        </pre>
+        <br>
+        
+        <div class='footer'>
+            <div class='link'>本首链接：$pageURL</div>
+            <div class='return'><a href='?n=i'>回目录</a></div>
+        </div>
+    </div>
+    </div>
+EOL;
 }

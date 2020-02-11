eee7cea7e9bbf33e8eabaefd9daa3dcf8897e7b1
esojourn <esojourn@gmail.com>
2020-02-11 19:57:42 +0800

    add 165 hymns from redbook 400

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-ini_set('display_errors', 1); 
-ini_set('display_startup_errors', 1); 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 include 'lib.php';
- 
+
 print_html_header();
 
 $path    = 'data';
@@ -14,52 +14,53 @@ $data = array_diff(scandir($path), array('.', '..'));
 
 $name = array();
 $audio = array();
+$jpg = array();
 
 foreach ($data as $key => $value) {
     $filesAll[] = $value;
-    if(substr(mb_strtolower($value), -4) == '.txt'){
+    if (substr(mb_strtolower($value), -4) == '.txt' || substr(mb_strtolower($value), -4) == '.jpg') {
         $name[] = get_name_array($value, $path);
     }
 }
 
-foreach ($data as $key => $value) {  
-//上面第一次处理 .txt 文件。据此得出文件列表。
-//下面得出 .mp3 文件列表。用于比对是否有对应音频文件。
+foreach ($data as $key => $value) {
+    //上面第一次处理 .txt 文件。据此得出文件列表。
+    //下面得出 .mp3 文件列表。用于比对是否有对应音频文件。
     if (substr(mb_strtolower($value), -4) == '.mp3') {
         $audio[] = get_name_array($value, $path);
     }
 }
 
+
 $output = get_html_contenttable($name, $audio);
 
-// echo '<pre>';
-// var_dump($name);
+ echo '<pre>';
+ //var_dump($name);
 // var_dump($audio);
-// echo '</pre>';
+ echo '</pre>';
 
-if (isset($_GET["n"])){
-    $n=htmlspecialchars(strip_tags($_GET["n"]));
-}else{
-    $n='';
+if (isset($_GET["n"])) {
+    $n = htmlspecialchars(strip_tags($_GET["n"]));
+} else {
+    $n = '';
 }
 //$n=(isset($_GET["n"])) ? htmlspecialchars($_GET["n"]) : NULL;
 
-$playkey=is_valid_id($name, $n);
+$playkey = is_valid_id($name, $n);
 //var_dump($playkey);
 
 preg_match("/^[0-9]{2}-[0-9]{2,3}/", $n);
 
-if($n=='index' || $n=='i' || $n==''){
+if ($n == 'index' || $n == 'i' || $n == '') {
     echo '<div class="main">';
     echo $output;
     echo '</div>';
-}elseif($playkey !== FALSE){ //如果ID符合格式
+} elseif ($playkey !== FALSE) { //如果ID符合格式
     print_html_player($name[$playkey]);
-}elseif($n=='check'){
+} elseif ($n == 'check') {
     check_duplication($name);
-}else{
+} else {
     echo '<h1>无权访问</h1>';
 }
 
 print_html_footer();
-?>
--- lib.php
+++ lib.php
@@ -2,9 +2,10 @@
 /*
  * 根据目录中所有文件，整理出格式化的数组。
 */
-function get_name_array($value, $path){
+function get_name_array($value, $path)
+{
     $filesTxt[] = $value;
-    preg_match("/^[0-9]{2}-[0-9]{2,3}/", $value, $arr);
+    preg_match("/^[0-9]{2}-[0-9]{1,3}/", $value, $arr);
     $id = $arr[0];
     $subid = explode('-', $id);
 
@@ -15,6 +16,7 @@ function get_name_array($value, $path){
         'name' => preg_replace('/\.[a-z|A-Z|0-9]{3}$/', '', ltrim($value, $id)),  // ltrim 去掉$id， preg_replace去掉扩展名
         'path' => $path . '/' . $value,
         'matched' => FALSE,
+        'format' => substr(mb_strtolower($value), -3),
     ];
     unset($arr, $subid, $id);
     return $name;
@@ -23,13 +25,14 @@ function get_name_array($value, $path){
 /*
  * 建立表格字符串。同时更新数组内 matched 字段信息。
 */
-function get_html_contenttable(&$name, $audio){
+function get_html_contenttable(&$name, $audio)
+{
     $output = '<table id="mytable"><thead><tr><th>目录</th><th>编号</th><th>诗歌</th></tr></thead><tbody>';
     foreach ($name as $k1 => $v1) {
         foreach ($audio as $k2 => $v2) {
-            if($name[$k1]['id']==$audio[$k2]['id']){  //如果txt文件有对应的mp3，则更新matched为true
+            if ($name[$k1]['id'] == $audio[$k2]['id']) {  //如果txt文件有对应的mp3，则更新matched为true
                 $name[$k1]['mp3'] = $audio[$k2]['path'];
-                $name[$k1]['matched'] =TRUE;
+                $name[$k1]['matched'] = TRUE;
             }
         }
         $output .= "<tr>\n";
@@ -45,12 +48,12 @@ function get_html_contenttable(&$name, $audio){
         $output .= "<td>";
         $output .= $v1['num'];
         $output .= "</td>\n";
-        
+
         $output .= "<td>\n";
         if ($name[$k1]['matched'] == TRUE) {
-            $output .= '<a href="?n='.$v1['id'].'">' .$v1['name']."</a>\n";
-        }else{
-            $output .= '<a href="?n='.$v1['id'].'">' .$v1['name']."</a> - 无伴奏\n";
+            $output .= '<a href="?n=' . $v1['id'] . '">' . $v1['name'] . "</a>\n";
+        } else {
+            $output .= '<a href="?n=' . $v1['id'] . '">' . $v1['name'] . "</a> - 无伴奏\n";
         }
         $output .= "</td></tr>\n";
     }
@@ -80,9 +83,10 @@ EOL;
 }
 
 
-function is_valid_id($name, $n){
+function is_valid_id($name, $n)
+{
     foreach ($name as $key => $value) {
-        if($n == $value['id']){
+        if ($n == $value['id']) {
             return $key;
         }
     }
@@ -90,70 +94,73 @@ function is_valid_id($name, $n){
 }
 
 
-function print_html_header(){
+function print_html_header()
+{
     echo <<<EOL
     <!DOCTYPE html>  
-
     <html lang="zh-CN" class="no-js">
     <head>  
         <meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.5, minimum-scale=0.6, user-scalable=yes"/>
-
-
+	    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.5, minimum-scale=0.6, user-scalable=yes"/>
         <title>敬拜赞美</title>  
-
         <link rel="stylesheet" type="text/css" href="styles.css"/>     
         <link rel="stylesheet" type="text/css" href="DataTables-1.10.20/css/jquery.dataTables.min.css"/>
-         
         <script type="text/javascript" src="jQuery-3.3.1/jquery-3.3.1.min.js"></script>
         <script type="text/javascript" src="DataTables-1.10.20/js/jquery.dataTables.min.js"></script>
-
     </head>  
     <body>
 EOL;
 }
 
 
-function get_page_url() {
-  $pageURL = 'http';
-  if(isset($_SERVER["HTTPS"])){
-       if ($_SERVER["HTTPS"] == "on") 
-      {
-        $pageURL .= "s";
-      }
-  }
-  $pageURL .= "://";
-  if ($_SERVER["SERVER_PORT"] != "80") 
-  {
-    $pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
-  } 
-  else
-  {
-    $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
-  }
-  return $pageURL;
+function get_page_url()
+{
+    $pageURL = 'http';
+    if (isset($_SERVER["HTTPS"])) {
+        if ($_SERVER["HTTPS"] == "on") {
+            $pageURL .= "s";
+        }
+    }
+    $pageURL .= "://";
+    if ($_SERVER["SERVER_PORT"] != "80") {
+        $pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
+    } else {
+        $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+    }
+    return $pageURL;
 }
 
-function print_html_player($name){
+function print_html_player($name)
+{
     //var_dump($name);
     $mp3 = isset($name["mp3"]) ? $name["mp3"] : '';
     $id = $name["id"];
-    $pageURL = get_page_url() ;
+    $pageURL = get_page_url();
+    $loop = '';
 
-    $file = file_get_contents($name["path"]);
-
-    $encode = mb_detect_encoding($file, array("ASCII",'UTF-8',"GB2312","GBK",'BIG5','LATIN1'));
-    if($encode != 'UTF-8'){
-        $file = mb_convert_encoding($file, 'UTF-8', $encode);
+    if($name['folder'] == '05'){
+        $loop = 'loop';
     }
 
     echo <<<EOL
     <div class='main'>
-        <div class='audio clearfix'>
-        <audio controls autoplay>  
-            <source src="$mp3" type="audio/mpeg">  
-        </audio>  
-    </div>
+    <div class='audio clearfix'>
+    <audio controls autoplay $loop>  
+        <source src="$mp3" type="audio/mpeg">  
+    </audio>  
+</div>
+EOL;
+
+    if ($name["format"] == 'txt') {
+        $file = file_get_contents($name["path"]);
+
+        $encode = mb_detect_encoding($file, array("ASCII", 'UTF-8', "GB2312", "GBK", 'BIG5', 'LATIN1'));
+        if ($encode != 'UTF-8') {
+            $file = mb_convert_encoding($file, 'UTF-8', $encode);
+        }
+
+        echo <<<EOL
+
 
     <div class='text'><div class='text-inner'><pre>
         $file
@@ -167,9 +174,22 @@ function print_html_player($name){
     </div>
     </div>
 EOL;
+    } elseif ($name["format"] == 'jpg') {
+        $file = $name["path"];
+        echo <<<EOL
+        <div class='text'><div class='text-inner'><img src=$file />
+            <div class='footer'>
+                <div class='link'>本首链接：$pageURL</div>
+                <div class='return'><a href='?n=i'>回目录</a></div>
+            </div>
+        </div>
+        </div>
+EOL;
+    }
 }
 
-function print_html_footer(){
+function print_html_footer()
+{
     echo <<<EOL
     <div style="text-align: center; padding: 10px 0 20px 0;"> <a href="https://jinshuju.net/f/bQvJ9p">故障报告</a></div>
     </body>
@@ -177,7 +197,8 @@ function print_html_footer(){
 EOL;
 }
 
-function check_duplication($arrays) {
+function check_duplication($arrays)
+{
     echo "<pre>find completely identical arrays\n";
     foreach ($arrays as $current_key => $current_array) {
         $search_key = array_search($current_array, $arrays);
@@ -188,19 +209,18 @@ function check_duplication($arrays) {
         }
         //echo "\n";
     }
-    
+
     echo "\n\nfind arrays with duplicate value for 'name'\n";
     foreach ($arrays as $current_key => $current_array) {
-        
+
         foreach ($arrays as $search_key => $search_array) {
             if ($search_array['name'] == $current_array['name']) {
                 if ($search_key != $current_key) {
-                    echo "value: " . $search_array['id'] . ' - ' . $search_array['name']. "\n";
+                    echo "value: " . $search_array['id'] . ' - ' . $search_array['name'] . "\n";
                     echo "current key: $current_key\n";
                     echo "duplicate found: $search_key\n";
                 }
             }
-
         }
         //echo "\n";
     }
@@ -210,12 +230,11 @@ function check_duplication($arrays) {
         foreach ($arrays as $search_key => $search_array) {
             if ($search_array['id'] == $current_array['id']) {
                 if ($search_key != $current_key) {
-                    echo "value: " . $search_array['id'] . ' - ' . $search_array['name']. "\n";
+                    echo "value: " . $search_array['id'] . ' - ' . $search_array['name'] . "\n";
                     echo "current key: $current_key\n";
                     echo "duplicate found: $search_key\n";
                 }
             }
-
         }
         //echo "\n";
     }

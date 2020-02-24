ca3e075b8aa19831d30c3e908a8aa2063b9294bd
esojourn <esojourn@gmail.com>
2020-02-24 08:30:23 +0800

    remove port 443 from footer display

--- lib.php
+++ lib.php
@@ -138,7 +138,7 @@ function get_page_url()
         }
     }
     $pageURL .= "://";
-    if ($_SERVER["SERVER_PORT"] != "80") {
+    if ($_SERVER["SERVER_PORT"] != "80" && $_SERVER["SERVER_PORT"] != "443") {
         $pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
     } else {
         $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
